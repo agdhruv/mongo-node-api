4ad3994de2bcf9cdc5d598f0a492e983724f4f67
agdhruv <dhruv_agarwal@live.com>
2017-10-07 01:40:02 +0530

    Connect to db\!

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,3 +1,11 @@
 const MongoClient = require('mongodb').MongoClient; // client connects to the Mongo Server
 
-MongoClient.connect();
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB server'); // return so that the rest of the function doesn't run
+	}
+
+	console.log('Connected to MongoDB server');
+
+	db.close();
+});

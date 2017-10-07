c73076040c07daf528833c3c5810e1092fe63f2a
agdhruv <dhruv_agarwal@live.com>
2017-10-08 01:53:30 +0530

    Insert document in collection

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,11 +1,32 @@
 const MongoClient = require('mongodb').MongoClient; // client connects to the Mongo Server
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-	if (err) {
+	if (err) { // check if error argument exists
 		return console.log('Unable to connect to MongoDB server'); // return so that the rest of the function doesn't run
 	}
 
 	console.log('Connected to MongoDB server');
+
+	// db.collection('Todos').insertOne({
+	// 	text: 'Something to do',
+	// 	comepleted: false
+	// }, (err, result) => {
+	// 	if (err) {
+	// 		return console.log('Unable to insert todo', err);
+	// 	}
+	// 	console.log(JSON.stringify(result.ops, undefined, 2)); // result.ops contains the documents that were added
+	// });
+
+	db.collection('Users').insertOne({
+		name: 'Dhruv',
+		age: 19,
+		location: 'Delhi'
+	}, (err, result) => {
+		if (err) {
+			return console.log('Unable to insert user', err);
+		}
+		console.log(JSON.stringify(result.ops, undefined, 2));
+	});
 
 	db.close();
 });

b9fc19bb975dd5b5382d90f1463c87ecc3ded3a6
agdhruv <dhruv_agarwal@live.com>
2017-10-09 01:14:50 +0530

    Start with deletion

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,13 @@
+// const MongoClient = require('mongodb').MongoClient; // client connects to the Mongo Server
+const {MongoClient, ObjectID} = require('mongodb'); // using object destructuring feature in ES6 - similar to code above
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) { // check if error argument exists
+		return console.log('Unable to connect to MongoDB server'); // return so that the rest of the function doesn't run
+	}
+
+	console.log('Connected to MongoDB server');
+
+
+	// db.close();
+});

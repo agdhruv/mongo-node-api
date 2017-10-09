84e8a6419e89697324a054ab390bd87402bea857
agdhruv <dhruv_agarwal@live.com>
2017-10-09 22:28:40 +0530

    Learn object destructuring

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient; // client connects to the Mongo Server
+// const MongoClient = require('mongodb').MongoClient; // client connects to the Mongo Server
+const {MongoClient, ObjectID} = require('mongodb'); // using object destructuring feature in ES6 - similar to code above
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	if (err) { // check if error argument exists
@@ -17,16 +18,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	// 	console.log(JSON.stringify(result.ops, undefined, 2)); // result.ops contains the documents that were added
 	// });
 
-	db.collection('Users').insertOne({
-		name: 'Dhruv',
-		age: 19,
-		location: 'Delhi'
-	}, (err, result) => {
-		if (err) {
-			return console.log('Unable to insert user', err);
-		}
-		console.log(JSON.stringify(result.ops, undefined, 2));
-	});
+	// db.collection('Users').insertOne({
+	// 	name: 'Dhruv',
+	// 	age: 19,
+	// 	location: 'Delhi'
+	// }, (err, result) => {
+	// 	if (err) {
+	// 		return console.log('Unable to insert user', err);
+	// 	}
+	// 	console.log(result.ops[0]._id.getTimestamp());
+	// });
 
 	db.close();
 });

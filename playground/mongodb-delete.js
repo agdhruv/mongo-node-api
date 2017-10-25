17a43ceec67d3cf46bd06629444db29d4e4d2012
agdhruv <dhruv_agarwal@live.com>
2017-10-26 03:02:48 +0530

    Delete documents from collection

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -8,6 +8,32 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
 	console.log('Connected to MongoDB server');
 
+	// deleteMany
+	// db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// deleteOne
+	// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// findOneAndDelete
+	// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// db.collection('Users').deleteMany({name: 'Dhruv'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	db.collection('Users').findOneAndDelete({
+		_id: new ObjectID("59d7e59ff41b9a3bc09cb62c")
+	}).then((result) => {
+		console.log(result);
+	}, (err) => {
+		console.log(err);
+	});
 
 	// db.close();
 });
